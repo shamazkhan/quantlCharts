d8285a910256d8a56d85ba95fac68db5c2f7895d
shamaz_khan <shamaz.khan@outlook.com>
2024-01-15 02:19:20 +0400

    changed node_env to production

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+dotenv.config({ path: `.env.${process.env.NODE_ENV || 'production'}.local` });
 
 const checkEnv = (envVar: string, defaultValue?: string) => {
   if (!process.env[envVar]) {
