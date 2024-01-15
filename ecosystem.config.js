bb528fc26043d3162b57a967bcaac5ed75972dde
shamaz_khan <shamaz.khan@outlook.com>
2024-01-15 23:50:59 +0400

    setting port to 8000

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       max_memory_restart: '1G',
       merge_logs: true,
       env: {
-        PORT: 3000,
+        PORT: 8000,
         NODE_ENV: 'production',
       },
     },
