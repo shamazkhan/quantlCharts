4463d77501c438c8fdf4181c8ead1146e8dc8c56
Yusra Masood <yusramasood@Yusras-MacBook-Pro.local>
2024-02-16 15:36:40 +0400

    Changed port to 3000 (hardcoded)

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,7 +14,7 @@ const checkEnv = (envVar: string, defaultValue?: string) => {
 };
 
 //export const PORT: number = parseInt(checkEnv('PORT'), 10);
-export const PORT: number = 3000;
+export const PORT: number = parseInt(process.env.PORT);
 export const MONGO_URI: string = checkEnv('MONGO_URI');
 export const DATABASE_GLD_SPY: string = checkEnv('DATABASE_GLD_SPY');
 export const DATABASE_XLE_TAN: string = checkEnv('DATABASE_XLE_TAN');
