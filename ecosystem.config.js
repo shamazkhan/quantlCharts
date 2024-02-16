804f8c1cbe5b5b0e6321ea38253849a92d739b33
Yusra Masood <yusramasood@Yusras-MacBook-Pro.local>
2024-02-16 17:57:36 +0400

    Disabled health check

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
     production: {
       user: 'SSH_USERNAME',
       host: '127.0.0.1',
-      ref: 'origin/main',
+      ref: 'origin/masters',
       repo: 'GIT_REPOSITORY',
       path: 'DESTINATION_PATH',
       'pre-deploy-local': '',
